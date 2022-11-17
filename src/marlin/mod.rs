62e3bb3e0ff1322ff3e6a45f628992d76b65d2df
Eduardo Damian Veralli <edveralli@gmail.com>
2022-11-17 18:41:34 -0300

    documentation added

--- src/marlin/mod.rs
+++ src/marlin/mod.rs
@@ -14,15 +14,38 @@ pub type MarlinInst = Marlin<Fr, MultiPC, FS>;
 pub type UniversalSRS = ark_marlin::UniversalSRS<Fr, MultiPC>;
 pub type ConstraintSystemRef = ark_relations::r1cs::ConstraintSystemRef<Fr>;
 
+/// Return a cryptographically secure random number generator that uses the ChaCha algorithm.
+/// 
 pub fn generate_rand() -> StdRng {
     ark_std::test_rng()
 }
 
+///  Generate the universal prover and verifier keys for the argument system.
+/// 
+/// # Parameters.
+/// - `rng` - A pseudorandom number generator (PRNG).
+/// 
+/// # Errors.
+/// Send the literal 'Error generating universal srs'.
+/// 
 pub fn generate_universal_srs(rng: &mut StdRng) -> Result<UniversalSRS> {
     MarlinInst::universal_setup(100000, 25000, 300000, rng)
         .map_err(|_e| anyhow!("Error generating universal srs"))
 }
 
+///  Return a serialized version of the verifying key and proving key generated with Marlin and the universal_srs.
+/// 
+/// # Parameters.
+/// - `universal_srs` - A universal prover and verifier keys for the argument system.
+/// - `rng` - A pseudorandom number generator (PRNG).
+/// - `constraint_system` - A shared reference to a constraint system that can be stored in high level variables.
+/// 
+/// # Errors
+/// Literal 'Error in index_from_constraint_system' when the Marlin generation of the keys fail.
+/// Literal 'Error in prove_from_constraint_system' when the Marlin proof of the proving key fail.
+/// Literal 'Error serializing proof' when the process of serialization of the proving key fail.
+/// Literal 'Error serializing verifying_key' when the process of serialization of the verifying key fail.
+/// 
 pub fn generate_proof(
     universal_srs: &UniversalSRS,
     rng: &mut StdRng,
