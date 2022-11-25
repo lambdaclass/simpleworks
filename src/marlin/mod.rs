27a017d7e8db463048e447b259fd2b4af32b4a9e
Javier Chatruc <jrchatruc@gmail.com>
2022-11-25 12:49:03 -0300

    [WIP] Verify function

--- src/marlin/mod.rs
+++ src/marlin/mod.rs
@@ -1,6 +1,8 @@
 use anyhow::{anyhow, Result};
-use ark_bls12_381::{Bls12_381, Fr};
-use ark_marlin::{Marlin, SimpleHashFiatShamirRng};
+use ark_bls12_381::{Bls12_381, Fr, FrParameters, Parameters};
+use ark_ec::bls12::Bls12;
+use ark_ff::Fp256;
+use ark_marlin::{IndexProverKey, IndexVerifierKey, Marlin, Proof, SimpleHashFiatShamirRng};
 use ark_poly::univariate::DensePolynomial;
 use ark_poly_commit::marlin_pc::MarlinKZG10;
 use ark_serialize::CanonicalSerialize;
@@ -13,6 +15,19 @@ pub type FS = SimpleHashFiatShamirRng<Blake2s, ChaChaRng>;
 pub type MarlinInst = Marlin<Fr, MultiPC, FS>;
 pub type UniversalSRS = ark_marlin::UniversalSRS<Fr, MultiPC>;
 pub type ConstraintSystemRef = ark_relations::r1cs::ConstraintSystemRef<Fr>;
+pub type VerifyingKey = IndexVerifierKey<
+    Fp256<FrParameters>,
+    MarlinKZG10<Bls12<Parameters>, DensePolynomial<Fp256<FrParameters>>>,
+>;
+pub type ProvingKey = IndexProverKey<
+    Fp256<FrParameters>,
+    MarlinKZG10<Bls12<Parameters>, DensePolynomial<Fp256<FrParameters>>>,
+>;
+pub type MarlinProof =
+    Proof<Fr, MarlinKZG10<Bls12<Parameters>, DensePolynomial<Fp256<FrParameters>>>>;
+use ark_serialize::CanonicalDeserialize;
+
+use crate::types::value::SimpleworksValueType;
 
 pub fn generate_rand() -> StdRng {
     ark_std::test_rng()
@@ -43,4 +58,16 @@ pub fn generate_proof(
         .map_err(|_e| anyhow!("Error serializing proof"))?;
 
     Ok(bytes_proof)
+}
+
+pub fn verify_proof(
+    verifying_key_serialized: Vec<u8>,
+    public_inputs: &[SimpleworksValueType],
+    proof_serialized: Vec<u8>,
+) -> Result<bool> {
+    let verifying_key = VerifyingKey::deserialize(&mut verifying_key_serialized.as_slice())?;
+    let proof = MarlinProof::deserialize(&mut proof_serialized.as_slice())?;
+    let rng = &mut ark_std::test_rng();
+
+    Ok(MarlinInst::verify(&verifying_key, &public_inputs, &proof, rng).unwrap())
 }
