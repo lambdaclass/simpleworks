8e99422c3314d0581f200dce9d5ac8ec42fcacef
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2022-11-25 15:09:11 -0300

    Record removed (#18)
    Co-authored-by: Pablo Deymonnaz <deymonnaz@gmail.com>

--- src/gadgets/mod.rs
+++ src/gadgets/mod.rs
@@ -3,9 +3,6 @@ use ark_r1cs_std::uint64::UInt64;
 mod address;
 pub use address::Address;
 
-pub mod record;
-pub use record::Record;
-
 pub type ConstraintF = ark_ed_on_bls12_381::Fq;
 
 pub type UInt64Gadget = UInt64<ConstraintF>;
