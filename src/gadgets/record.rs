cc6505722e488c63a405c6b952ef4fadb756cd50
Iván Litteri <ilitteri@fi.uba.ar>
2022-11-25 14:35:59 -0300

    Made Record attr public so we can cut the methods

--- src/gadgets/record.rs
+++ src/gadgets/record.rs
@@ -2,20 +2,6 @@ use super::{UInt64Gadget, AddressGadget};
 
 #[derive(Clone, Debug)]
 pub struct Record {
-    address: AddressGadget,
-    gates: UInt64Gadget,
-}
-
-impl Record {
-    pub fn new(address: AddressGadget, gates: UInt64Gadget) -> Self {
-        Self { address, gates }
-    }
-
-    pub fn gates(&self) -> &UInt64Gadget {
-        &self.gates
-    }
-
-    pub fn address(&self) -> &AddressGadget {
-        &self.address
-    }
+    pub address: AddressGadget,
+    pub gates: UInt64Gadget,
 }
