fdc06ff5fd440a110c3131fcf4eda04bf6e03b81
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2022-12-01 14:47:02 -0300

    Proof Generation returns the concrete proof instead of a deserialized one (#24)
    * Proof gen returns the proof instead of a Vec<u8>
    
    * Add serde for proofs
    
    * cargo fmt

--- src/marlin/mod.rs
+++ src/marlin/mod.rs
@@ -5,7 +5,7 @@ use ark_ff::Fp256;
 use ark_marlin::{IndexProverKey, IndexVerifierKey, Marlin, Proof, SimpleHashFiatShamirRng};
 use ark_poly::univariate::DensePolynomial;
 use ark_poly_commit::marlin_pc::MarlinKZG10;
-use ark_serialize::CanonicalSerialize;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use blake2::Blake2s;
 use rand::rngs::StdRng;
 use rand_chacha::ChaChaRng;
@@ -64,16 +64,9 @@ pub fn generate_proof(
     constraint_system: ConstraintSystemRef,
     proving_key: ProvingKey,
     rng: &mut StdRng,
-) -> Result<Vec<u8>> {
-    let proof = MarlinInst::prove_from_constraint_system(&proving_key, constraint_system, rng)
-        .map_err(|_e| anyhow!("Error in prove_from_constraint_system"))?;
-
-    let mut bytes_proof = Vec::new();
-    proof
-        .serialize(&mut bytes_proof)
-        .map_err(|_e| anyhow!("Error serializing proof"))?;
-
-    Ok(bytes_proof)
+) -> Result<MarlinProof> {
+    MarlinInst::prove_from_constraint_system(&proving_key, constraint_system, rng)
+        .map_err(|_e| anyhow!("Error in prove_from_constraint_system"))
 }
 
 pub fn verify_proof(
@@ -91,4 +84,18 @@ pub fn generate_proving_and_verifying_keys(
 ) -> Result<(ProvingKey, VerifyingKey)> {
     MarlinInst::index_from_constraint_system(universal_srs, constraint_system)
         .map_err(|_e| anyhow!("Error in index_from_constraint_system"))
+}
+
+pub fn serialize_proof(proof: MarlinProof) -> Result<Vec<u8>> {
+    let mut bytes_proof = Vec::new();
+    proof
+        .serialize(&mut bytes_proof)
+        .map_err(|_e| anyhow!("Error serializing proof"))?;
+
+    Ok(bytes_proof)
+}
+
+pub fn deserialize_proof(bytes_proof: Vec<u8>) -> Result<MarlinProof> {
+    MarlinProof::deserialize(&mut bytes_proof.as_slice())
+        .map_err(|_e| anyhow!("Error deserializing proof"))
 }
