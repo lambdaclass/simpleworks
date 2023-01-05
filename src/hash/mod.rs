761577cef8efa6f198ed57fcb2146e4a125583a8
Iván Litteri <ilitteri@fi.uba.ar>
2023-01-05 13:05:28 -0300

    Implement native poseidon2 hash

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -5,6 +5,10 @@ use ark_crypto_primitives::{
     CRH,
 };
 use ark_ed_on_bls12_377::{EdwardsProjective, Fq};
+use ark_sponge::poseidon::PoseidonSponge;
+use ark_sponge::{CryptographicSponge, FieldBasedCryptographicSponge};
+
+pub mod helpers;
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 struct LeafWindow;
@@ -21,4 +25,19 @@ pub fn pedersen_hash(input: &[u8]) -> Result<Fq> {
     let params = PedersenHash::setup(&mut rng).map_err(|e| anyhow!("{:?}", e))?;
 
     PedersenHash::evaluate(&params, input).map_err(|e| anyhow!("{:?}", e))
+}
+
+type PoseidonHash = PoseidonSponge<Fq>;
+
+pub fn poseidon2_hash(input: &[u8]) -> Result<Fq> {
+    let sponge_params = helpers::poseidon_parameters_for_test()?;
+
+    let mut native_sponge = PoseidonHash::new(&sponge_params);
+
+    native_sponge.absorb(&input);
+    native_sponge
+        .squeeze_native_field_elements(1)
+        .first()
+        .ok_or_else(|| anyhow!("Error getting the first element of the input"))
+        .copied()
 }
