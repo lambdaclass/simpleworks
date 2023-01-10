8aa1bef4a3908b576229f5e92ae041ad1ef132e6
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2023-01-10 18:21:45 -0300

    Implement `BitsRotationGadget` for bit rotation (#54)
    * Define BitsRotationGadget trait
    
    * Impl BitsRotationGadget trait for Uint8
    
    * Impl BitsRotationGadget trait for Uint16
    
    * Impl BitsRotationGadget trait for Uint32
    
    * Impl BitsRotationGadget trait for Uint64
    
    * Impl BitsRotationGadget trait for Uint64
    
    * cargo fmt
    
    * Impl BitsRotationGadget trait for [Uint64; 4]
    
    * cargo fmt

--- src/gadgets/uint16.rs
+++ src/gadgets/uint16.rs
@@ -1,7 +1,8 @@
-use super::traits::{FromBytesGadget, IsWitness, ToFieldElements};
+use super::traits::{BitRotationGadget, FromBytesGadget, IsWitness, ToFieldElements};
 use anyhow::Result;
 use ark_ff::Field;
 use ark_r1cs_std::{prelude::Boolean, uint16::UInt16, uint8::UInt8, R1CSVar, ToBitsGadget};
+use ark_relations::{lc, r1cs::ConstraintSystemRef};
 
 impl<F: Field> ToFieldElements<F> for UInt16<F> {
     fn to_field_elements(&self) -> Result<Vec<F>> {
@@ -47,3 +48,109 @@ impl<F: Field> FromBytesGadget<F> for UInt16<F> {
 }
 
 impl<F: Field> IsWitness<F> for UInt16<F> {}
+
+impl<F: Field> BitRotationGadget<F> for UInt16<F> {
+    fn rotate_left(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        let mut primitive_bits = self.to_bits_le();
+        primitive_bits.reverse();
+        let mut rotated_bits = primitive_bits.clone();
+        rotated_bits.rotate_left(positions);
+
+        for i in 0..16 {
+            let a = &primitive_bits[(i + positions) % 16];
+            let b = &rotated_bits[i];
+            let c = lc!() + a.lc() - b.lc();
+            constraint_system.enforce_constraint(lc!(), lc!(), c)?
+        }
+
+        rotated_bits.reverse();
+        Ok(UInt16::<F>::from_bits_le(&rotated_bits))
+    }
+
+    fn rotate_right(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        // Example: rotate one place to the right is the same as rotate 7 places
+        // to the left while generating the same number of constraints.
+        // We compute positions % 16 to avoid subtraction overflow when someone
+        // tries to rotate more then 16 positions.
+        self.rotate_left(16 - (positions % 16), constraint_system)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gadgets::{traits::BitRotationGadget, ConstraintF, UInt16Gadget};
+    use ark_r1cs_std::{prelude::AllocVar, R1CSVar};
+    use ark_relations::r1cs::ConstraintSystem;
+
+    #[test]
+    fn test_one_left_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt16Gadget::new_witness(cs.clone(), || Ok(0xF000)).unwrap();
+        let positions_to_rotate = 1;
+        let expected_byte = byte.value().unwrap().rotate_left(positions_to_rotate);
+
+        let result = byte
+            .rotate_left(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 0xE001);
+    }
+
+    #[test]
+    fn test_more_than_one_left_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt16Gadget::new_witness(cs.clone(), || Ok(0xF000)).unwrap();
+        let positions_to_rotate = 2;
+        let expected_byte = byte.value().unwrap().rotate_left(positions_to_rotate);
+
+        let result = byte
+            .rotate_left(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 0xC003);
+    }
+
+    #[test]
+    fn test_one_right_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt16Gadget::new_witness(cs.clone(), || Ok(0xC003)).unwrap();
+        let positions_to_rotate = 1;
+        let expected_byte = byte.value().unwrap().rotate_right(positions_to_rotate);
+
+        let result = byte
+            .rotate_right(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 0xE001);
+    }
+
+    #[test]
+    fn test_more_than_one_right_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt16Gadget::new_witness(cs.clone(), || Ok(0xC003)).unwrap();
+        let positions_to_rotate = 2;
+        let expected_byte = byte.value().unwrap().rotate_right(positions_to_rotate);
+
+        let result = byte
+            .rotate_right(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 0xF000);
+    }
+}
--- src/gadgets/uint8.rs
+++ src/gadgets/uint8.rs
@@ -1,7 +1,8 @@
-use super::traits::{IsWitness, ToFieldElements};
+use super::traits::{BitRotationGadget, IsWitness, ToFieldElements};
 use anyhow::Result;
 use ark_ff::Field;
 use ark_r1cs_std::{uint8::UInt8, R1CSVar, ToBitsGadget};
+use ark_relations::{lc, r1cs::ConstraintSystemRef};
 
 impl<F: Field> ToFieldElements<F> for UInt8<F> {
     fn to_field_elements(&self) -> Result<Vec<F>> {
@@ -20,3 +21,150 @@ impl<F: Field> ToFieldElements<F> for UInt8<F> {
 }
 
 impl<F: Field> IsWitness<F> for [UInt8<F>] {}
+
+impl<F: Field> BitRotationGadget<F> for UInt8<F> {
+    fn rotate_left(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        let primitive_bits = self.to_bits_be()?;
+        let mut rotated_bits = primitive_bits.clone();
+        rotated_bits.rotate_left(positions);
+
+        for i in 0..8 {
+            let a = &primitive_bits[(i + positions) % 8];
+            let b = &rotated_bits[i];
+            let c = lc!() + a.lc() - b.lc();
+            constraint_system.enforce_constraint(lc!(), lc!(), c)?
+        }
+
+        rotated_bits.reverse();
+        Ok(UInt8::<F>::from_bits_le(&rotated_bits))
+    }
+
+    fn rotate_right(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        // Example: rotate one place to the right is the same as rotate 7 places
+        // to the left while generating the same number of constraints.
+        // We compute positions % 8 to avoid subtraction overflow when someone
+        // tries to rotate more then 8 positions.
+        self.rotate_left(8 - (positions % 8), constraint_system)
+    }
+}
+
+impl<F: Field> BitRotationGadget<F> for [UInt8<F>; 4] {
+    fn rotate_left(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        let primitive_bits = self.to_bits_be()?;
+        let mut rotated_bits = primitive_bits.clone();
+        rotated_bits.rotate_left(positions);
+
+        for i in 0..self.len() {
+            let a = &primitive_bits[(i + positions) % self.len()];
+            let b = &rotated_bits[i];
+            let c = lc!() + a.lc() - b.lc();
+            constraint_system.enforce_constraint(lc!(), lc!(), c)?
+        }
+
+        rotated_bits.reverse();
+        let mut result = [
+            UInt8::<F>::constant(0),
+            UInt8::<F>::constant(0),
+            UInt8::<F>::constant(0),
+            UInt8::<F>::constant(0),
+        ];
+        for (result_byte, result_chunk_byte) in result.iter_mut().zip(rotated_bits.chunks(8)) {
+            *result_byte = UInt8::<F>::from_bits_le(result_chunk_byte);
+        }
+
+        Ok(result)
+    }
+
+    fn rotate_right(
+        &self,
+        positions: usize,
+        constraint_system: ConstraintSystemRef<F>,
+    ) -> Result<Self> {
+        // Example: rotate one place to the right is the same as rotate 7 places
+        // to the left while generating the same number of constraints.
+        // We compute positions % 8 to avoid subtraction overflow when someone
+        // tries to rotate more then 8 positions.
+        self.rotate_left(self.len() - (positions % self.len()), constraint_system)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gadgets::{traits::BitRotationGadget, ConstraintF, UInt8Gadget};
+    use ark_r1cs_std::{prelude::AllocVar, R1CSVar};
+    use ark_relations::r1cs::ConstraintSystem;
+
+    #[test]
+    fn test_one_left_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt8Gadget::new_witness(cs.clone(), || Ok(142)).unwrap();
+        let positions_to_rotate = 1;
+        let expected_byte = byte.value().unwrap().rotate_left(positions_to_rotate);
+
+        let result = byte
+            .rotate_left(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+    }
+
+    #[test]
+    fn test_more_than_one_left_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt8Gadget::new_witness(cs.clone(), || Ok(142)).unwrap();
+        let positions_to_rotate = 2;
+        let expected_byte = byte.value().unwrap().rotate_left(positions_to_rotate);
+
+        let result = byte
+            .rotate_left(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+    }
+
+    #[test]
+    fn test_one_right_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt8Gadget::new_witness(cs.clone(), || Ok(135)).unwrap();
+        let positions_to_rotate = 1;
+        let expected_byte = byte.value().unwrap().rotate_right(positions_to_rotate);
+
+        let result = byte
+            .rotate_right(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 195);
+    }
+
+    #[test]
+    fn test_more_than_one_right_rotation() {
+        let cs = ConstraintSystem::<ConstraintF>::new_ref();
+        let byte = UInt8Gadget::new_witness(cs.clone(), || Ok(135)).unwrap();
+        let positions_to_rotate = 2;
+        let expected_byte = byte.value().unwrap().rotate_right(positions_to_rotate);
+
+        let result = byte
+            .rotate_right(positions_to_rotate.try_into().unwrap(), cs.clone())
+            .unwrap();
+
+        assert!(cs.is_satisfied().unwrap());
+        assert_eq!(expected_byte, result.value().unwrap());
+        assert_eq!(result.value().unwrap(), 225);
+    }
+}
