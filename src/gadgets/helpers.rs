47b96ad7048ef895e70931661b9f8e7e531b42a3
IAvecilla <iavecilla@fi.uba.ar>
2023-01-31 16:15:30 -0300

    Add comparison helper and enum with different comparison types

--- src/gadgets/helpers.rs
+++ src/gadgets/helpers.rs
@@ -1,9 +1,31 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use ark_ff::Field;
-use ark_r1cs_std::prelude::Boolean;
-use ark_relations::r1cs::SynthesisError;
+use ark_r1cs_std::{
+    prelude::{AllocVar, Boolean},
+    select::CondSelectGadget,
+    R1CSVar,
+};
+use ark_relations::r1cs::{ConstraintSystemRef, SynthesisError};
 
-pub fn zip_bits_and_apply<F: Field, T>(
+pub enum Comparison {
+    GreaterThan,
+    GreaterThanOrEqual,
+    LessThanOrEqual,
+    LessThan,
+}
+
+impl Comparison {
+    pub fn instruction(&self) -> &str {
+        match self {
+            Comparison::GreaterThan => "gt",
+            Comparison::GreaterThanOrEqual => "gte",
+            Comparison::LessThanOrEqual => "lte",
+            Comparison::LessThan => "lt",
+        }
+    }
+}
+
+pub(crate) fn zip_bits_and_apply<F: Field, T>(
     first_bits_to_iterate: Vec<Boolean<F>>,
     second_bits_to_iterate: Vec<Boolean<F>>,
     function_to_apply: T,
@@ -21,4 +43,31 @@ where
         result.push(operation_result);
     }
     Ok(result)
+}
+
+pub(crate) fn compare_ord<F: Field, T: R1CSVar<F>>(
+    left_operand: T,
+    right_operand: T,
+    comparison: Comparison,
+    constraint_system: ConstraintSystemRef<F>,
+) -> Result<Boolean<F>>
+where
+    T::Value: PartialOrd,
+{
+    let result = match comparison {
+        Comparison::GreaterThan => left_operand.value()? > right_operand.value()?,
+        Comparison::GreaterThanOrEqual => left_operand.value()? >= right_operand.value()?,
+        Comparison::LessThanOrEqual => left_operand.value()? <= right_operand.value()?,
+        Comparison::LessThan => left_operand.value()? < right_operand.value()?,
+    };
+
+    let true_witness = Boolean::<F>::new_witness(constraint_system.clone(), || Ok(true))?;
+    let false_witness = Boolean::<F>::new_witness(constraint_system.clone(), || Ok(false))?;
+
+    Boolean::conditionally_select(
+        &Boolean::new_witness(constraint_system, || Ok(result))?,
+        &true_witness,
+        &false_witness,
+    )
+    .map_err(|error| anyhow!(error))
 }
